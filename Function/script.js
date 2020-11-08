ac13f3cbd0a4a5261e1a8d868534853e8616e480
Stephenice <stephencode@hotmail.com>
2020-11-08 08:04:07 +0100

    Added F(x) calling another F(x)

--- Function/script.js
+++ Function/script.js
@@ -16,3 +16,20 @@ const age2 = calculateAge2(1991)
 console.log(`f() declaration Age is ${age}`);
 console.log(`f() expression Age is ${age2}`);
 
+
+// f(x) calling a f(x)
+// Dont repeat Yourself rule
+
+function fruitCut(fruit){
+    return 4 * fruit;
+}
+
+function fruits(apples, orange){
+    //calling fruitCut function inside fruits function
+    const applesPieces = fruitCut(apples);
+    const orangesPieces = fruitCut(orange);
+    const juices = `Apple pieces is ${applesPieces} and Orange pieces is ${orangesPieces}`;
+    return juices;
+}
+
+console.log(fruits(4, 5));
