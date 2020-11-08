6ad621fe39e003d2829a0782c64ef1ee4161dc03
Stephenice <stephencode@hotmail.com>
2020-11-08 06:48:18 +0100

    Added F(x) declaration and F(x) expression

--- Function/script.js
+++ Function/script.js
@@ -1,5 +1,18 @@
-//function
-function getAge(){
+//function declaration
+//Note: f(x) declaration can be call or invoke before the f(x) because of Hositing
+const age= calculateAge(1990);
 
+function calculateAge(birthYear){
+    return 2020 - birthYear;
 }
+
+//function expression produce result
+const calculateAge2 = function (birthYear){
+    return 2020 - birthYear;
+}
+
+const age2 = calculateAge2(1991)
+
+console.log(`f() declaration Age is ${age}`);
+console.log(`f() expression Age is ${age2}`);
 
