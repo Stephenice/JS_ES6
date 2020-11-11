3253e9a57f593a7bbe3da67f655ead7613ccea35
Stephenice <stephencode@hotmail.com>
2020-11-11 14:52:04 +0100

    Added Object

---
+++ Object/script.js
@@ -0,0 +1,12 @@
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+  };
+
+  // Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
