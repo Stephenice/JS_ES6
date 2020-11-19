b6cd46a9bdc9de2e121aa8d0449252527e48f4ce
Stephenice <stephencode@hotmail.com>
2020-11-19 12:17:54 +0100

    Added game guess

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,21 @@
 'use strict';
+// create a secret number or Random number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 
 //eventlistener for the check button
 document.querySelector('.check').addEventListener('click', function(){
+    let guessNumber = Number(document.querySelector('.guess').value);
 
-    
+    if(!guessNumber){
+        document.querySelector('.message').textContent = '🚫 No number ...'
+    } else if (guessNumber === secretNumber){
+        document.querySelector('.message').textContent = '🟢 👍 Correct Number ...'
+    }else if (guessNumber > secretNumber){
+        document.querySelector('.message').textContent = ' Too High ...'
+    }else if (guessNumber < secretNumber){
+        document.querySelector('.message').textContent = ' Too Low ...'
+    }
+
 })
