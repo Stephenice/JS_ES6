06745d919cea49949449a5aeb639131a93537ecb
Stephenice <stephencode@hotmail.com>
2020-12-19 01:13:33 +0100

    Destructuring Array and Object

--- Coding Challenge9.1/script.js
+++ Coding Challenge9.1/script.js
@@ -60,34 +60,22 @@ const game = {
 
 
   //1. Create one player array for each team (variables 'players1' and 'players2')
-  const [players1, players2] = game.players;
-  console.log(players1,players2);
+
 
   //2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-  const [gk, ...fieldPlayers] = players1;
-  console.log(gk, fieldPlayers);
+
 
   //3. Create an array 'allPlayers' containing all players of both teams (22 players)
-  const allPlayers = [...players1, ...players2];
-  console.log(allPlayers);
+  
 
   //4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 
-  const players1Final = ['Thiago', 'Coutinho','Perisic', ...players1];
-  console.log(players1Final);
-
+  
   //5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
- const {odds: { team1, x:draw, team2}} = game;
- console.log(team1,draw,team2);
+ 
 
   //6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-function printGoals (...playerScore){
-  console.log(playerScore);
-console.log(`${playerScore.length} goals were scored`);
-}
-printGoals(...game.scored);
+
 
 //7. 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
-team1 < team2 && console.log(`'Team 1 is more likely to win`);
-team1 > team2 && console.log(`'Team 2 is more likely to win`);
