73a597f64f5f25f15297b260329dca24edb07097
Stephenice <stephencode@hotmail.com>
2021-01-15 12:31:04 +0100

    higher order function

--- Higher_Order_Function/script.js
+++ Higher_Order_Function/script.js
@@ -1,1 +1,26 @@
 'use strict'
+const words = document.querySelector('textarea');
+const btn = document.querySelector('button');
+const cost = document.querySelector('.cost');
+//console.log(words);
+
+function wordsLength (words){
+    const wordCount = words.length;
+    costs(wordCount);
+}
+
+function costs (words){
+    const totalCost = words * 5
+    document.querySelector('.cost').innerHTML=totalCost;
+}
+
+function wordsFunction(){
+    const wordsFormat = words.value;
+    wordsLength(wordsFormat);
+    console.log(wordsFormat);
+}
+
+wordsFunction(words.value);
+
+btn.addEventListener('click', wordsFunction )
+
