57fbbe5f222927246cc56c49e45e6c33c0d3e294
Stephenice <stephencode@hotmail.com>
2021-03-10 07:52:06 +0100

    ch 11:4

--- Coding Challenge11.4/script.js
+++ Coding Challenge11.4/script.js
@@ -9,7 +9,9 @@ Eating too much means the dog's current food portion is larger than the recommen
 Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
 
 1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+
 2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+
 3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
 4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
 5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
@@ -30,3 +32,23 @@ const dogs = [
 
 GOOD LUCK 😀
 */
+
+const dogs = [
+    { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+    { weight: 8, curFood: 200, owners: ['Matilda'] },
+    { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+    { weight: 32, curFood: 340, owners: ['Michael'] }
+  ];
+
+  //1.
+  dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+  console.log(dogs);
+
+  //2.
+  const sarahDog = function(dog){
+  const myDog = dog.find(dg => dg.owners.includes('Sarah'));
+   console.log(`Sarah's dog is eating too ${myDog.curFood > myDog.recFood ? 'Much' : 'Little'}`);
+  }
+    sarahDog(dogs);
+
+  //3.
