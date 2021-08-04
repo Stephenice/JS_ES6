e0acd9f32cfa41b7d23a0c9f42ad637148a5bb19
Stephenice <stephencode@hotmail.com>
2021-08-04 15:22:11 +0200

    scrolling smoothly

--- 13-DOM-Bankist/starter/script.js
+++ 13-DOM-Bankist/starter/script.js
@@ -32,3 +32,14 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+//smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function(event){
+event.preventDefault;
+section1.scrollIntoView({behavior : 'smooth'});
+})
+
