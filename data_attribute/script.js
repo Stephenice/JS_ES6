4c45f663a6c322635ea06a1596e686445bab6da5
Stephenice <stephencode@hotmail.com>
2021-08-13 17:08:54 +0200

    Array.from

---
+++ data_attribute/script.js
@@ -0,0 +1,9 @@
+const all_lists = document.querySelectorAll("li");
+
+// Array.from its use to create array eg. from Object and from scratch
+const lists_array = Array.from(all_lists);
+
+console.log(lists_array);
+
+//looping array with forEach
+lists_array.forEach()
