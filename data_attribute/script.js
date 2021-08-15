a7a9b4a9c70df73af21865990d40f9ef4e30804f
Stephenice <stephencode@hotmail.com>
2021-08-15 06:03:41 +0200

    Apply css

--- data_attribute/script.js
+++ data_attribute/script.js
@@ -1,18 +1,17 @@
 const all_lists = document.querySelectorAll("li");
 
-// // Array.from its use to create array eg. from Object and from scratch
-// const lists_array = Array.from(all_lists);
-// console.log(lists_array);
 
 
 const species = function(arrayElement){ 
     console.log(arrayElement);
-    //Note:: NodeList doesnt support addEventListener so its must loop for applying addEventListener
+    //Note:: NodeList doesnt support addEventListener so its must be loop to apply addEventListener
     arrayElement.addEventListener('click', function(e){
         console.log(arrayElement.dataset.animalType);
+        alert(arrayElement.dataset.animalType);
+
     })
 }
 
 
-//looping array with forEach
+//looping the nodelist with forEach
 all_lists.forEach(species);
