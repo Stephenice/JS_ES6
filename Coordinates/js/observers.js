17690ca3b8343b41319a2bd6f1f94496a461cfad
Stephenice <stephencode@hotmail.com>
2021-09-06 12:15:05 +0200

    Creating a get_bounding_result function

--- Coordinates/js/observers.js
+++ Coordinates/js/observers.js
@@ -25,10 +25,27 @@
 //getBoundingClientRect
 const login = document.querySelector('.login');
 const header = document.querySelector("header");
+const resultElement = document.querySelector(".result");
 
 login.addEventListener('click', function(e){
     e.preventDefault;
-    const header = document.querySelector(".home-intro");
-    console.log(header.getBoundingClientRect());
-    console.log();
+    const header = document.querySelector(".result");
+    get_bounding_result(header);
 })
+
+function get_bounding_result(elementCoordinate){
+ console.log(elementCoordinate.getBoundingClientRect());
+ const result_coordinate = elementCoordinate.getBoundingClientRect();
+    const result_ul = document.createElement('ul');
+    result_ul.innerHTML = `<li>${result_coordinate.bottom}</li>
+                            <li>${result_coordinate.height}</li>
+                            <li>${result_coordinate.left}</li>
+                            <li>${result_coordinate.right}</li>
+                            <li>${result_coordinate.top}</li>
+                            <li>${result_coordinate.width}</li>
+                            <li>${result_coordinate.x}</li>
+                            <li>${result_coordinate.y}</li>`;
+
+     resultElement.prepend(result_ul);
+    
+};
