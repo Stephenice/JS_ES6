d39359a8f21fd0152115f05c9f336f3d7264bf0e
Stephenice <stephencode@hotmail.com>
2021-10-01 05:03:13 +0200

    OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const Person = function(firstName,lastName){
+    this.first = firstName;
+    this.last = lastName;
+
+    //never create method inside the f() 
+    //because of functioanilty
+}
+
+//towo object created
+const steve = new Person('Steve', 'bill');
+console.log(steve);
+const john = new Person('John', 'william');
+
+console.log(steve instanceof Person);
+console.log(steve instanceof Person);
