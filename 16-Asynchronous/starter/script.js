cc4a58ea148e207e1c94a2642da3848283f47413
stephenice <stephencode@hotmil.com>
2021-11-12 09:32:39 +0100

    API

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,31 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const countryName = 'nigeria';
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v2/name/${countryName}`);
+// ajax call- the request cant be set to a varible bcos of Asynchrous behavour
+request.send();
+
+// register a call-back
+request.addEventListener('load', function () {
+  //convert to json to js objects and destructuring
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = `<article class="country">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+});
